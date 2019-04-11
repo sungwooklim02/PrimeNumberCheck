8f9fa747db9246fba19c873259479a0c1a560261
dit06 <dit@imac-5.local>
2019-04-11 14:22:20 +0900

    Initial Commit

---
+++ PrimeNumberCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeNumberCheck
+//
+//  Created by dit06 on 2019. 4. 11..
+//  Copyright © 2019년 dit06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
