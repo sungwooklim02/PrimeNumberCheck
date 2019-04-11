a2664e9a2de8b5dd70e11e64c6ed6b611dde7700
dit06 <dit@imac-5.local>
2019-04-11 16:28:11 +0900

    commit

--- PrimeNumberCheck/ViewController.swift
+++ PrimeNumberCheck/ViewController.swift
@@ -8,13 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var txtNum: UITextField!
+    @IBOutlet weak var txtResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtNum.delegate = self;
+        txtNum.clearButtonMode = UITextField.ViewMode.always;
+        txtNum.placeholder = "숫자를 입력하시오";
+    }
+    @IBAction func btnCheck(_ sender: UIButton) {
+        
+        if let num = Int(txtNum.text!){
+            var isPrime = true;
+        
+        if num <= 0 {
+            txtResult.text = "input correct value!"
+            txtNum.text = ""
+            return
+        }
+            
+        
+        if num == 1{
+            isPrime = false
+        }
+            if (num != 1 && num != 2) {
+            for i in 2 ..< num {
+                if (num % i == 0){
+                    isPrime = false
+                }
+            }
+        }
+        
+        
+            if(isPrime == true){
+                txtResult.text = "This Number is prime number"
+            }
+            else if (isPrime == false){
+                txtResult.text = "This Number is not prime number"
+            }
+        
+        
+        txtNum.text = ""
+        txtNum.resignFirstResponder();
+        }else{
+            txtResult.text = "input correct value!"
+            txtNum.text = ""
+        }
+    }
+    
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool{ // called when 'return' key pressed. return NO to ignore.
+        self.view.backgroundColor = UIColor.green;
+        txtNum.resignFirstResponder();
+        return true;
     }
 
-
 }
-
